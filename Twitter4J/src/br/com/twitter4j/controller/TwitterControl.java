6269713adb4c9914ee6414a0c0819cb4678ffbad
MauricioJunior <MauricioJunior@192.168.0.13>
2016-10-31 19:27:53 -0200

    Update TwitterControll Class (Methods and Javadoc)

--- Twitter4J/src/br/com/twitter4j/controller/TwitterControl.java
+++ Twitter4J/src/br/com/twitter4j/controller/TwitterControl.java
@@ -17,8 +17,6 @@ import twitter4j.auth.AccessToken;
 import twitter4j.conf.Configuration;
 import twitter4j.conf.ConfigurationBuilder;
 
-
-
 public final class TwitterControl {
 
 	private final static String apiKey = "LJOe3Qg7ccVNfKkVPaP7Tf848";
@@ -26,33 +24,63 @@ public final class TwitterControl {
 	private final static String accessToken = "790601960890269696-ztYLHfDGufgAK312EZKgbq2aNiGutTW";
 	private final static String accessTokenSecret = "RIYAfooB8G9BNUEBrcMbDE63jUO7f1Cb9kxq3HaK4X2a1";
 
+	/**
+	 * Get Twitter object connection with credentials provided.
+	 * 
+	 * @return Twitter object already configured
+	 */
 	public Twitter setApiCredentials() {
 
-		ConfigurationBuilder builder = new ConfigurationBuilder();
-		builder.setOAuthConsumerKey(TwitterControl.apiKey);
-		builder.setOAuthConsumerSecret(TwitterControl.apiSecret);
+		Configuration configuration = loadApiKey().build();
+		AccessToken accessToken = loadAccessToken();
 
-		Configuration configuration = builder.build();
 		TwitterFactory factory = new TwitterFactory(configuration);
 		Twitter twitter = factory.getInstance();
-		AccessToken accessToken = loadAccessToken();
+
 		twitter.setOAuthAccessToken(accessToken);
 
 		return twitter;
 	}
 
+	/**
+	 * Configure builder with credentials provided.
+	 * 
+	 * @return ConfigurationBuilder object already configured
+	 */
+	private static ConfigurationBuilder loadApiKey() {
+
+		ConfigurationBuilder builder = new ConfigurationBuilder();
+		builder.setOAuthConsumerKey(TwitterControl.apiKey);
+		builder.setOAuthConsumerSecret(TwitterControl.apiSecret);
+
+		return builder;
+	}
+
+	/**
+	 * Configure access token with credentials provided.
+	 * 
+	 * @return AccessToken object already configured
+	 */
 	private static AccessToken loadAccessToken() {
 
 		return new AccessToken(TwitterControl.accessToken, TwitterControl.accessTokenSecret);
 	}
 
+	/**
+	 *  Return tweets made in the previous week
+	 * 
+	 * @param twitter Twitter object to make the connection
+	 * @param search String with what will be searched
+	 * @return QueryResult object containing result of the search 
+	 * @throws TwitterException
+	 */
 	public QueryResult searchTweetByWeek(Twitter twitter, String search) throws TwitterException {
 
 		LocalDate today = LocalDate.now();
 		LocalDate lastWeek = LocalDate.now().minusDays(1);
 		DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 		String s = lastWeek.format(dateTimeFormatter).concat("-").concat(today.format(dateTimeFormatter));
-	
+
 		Query query = new Query(search).since(s);
 		QueryResult result = twitter.search(query);
 
