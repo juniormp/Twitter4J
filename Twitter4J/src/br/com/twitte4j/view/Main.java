baa79fa857667cc6e1413099628ddcb0ebcc52c2
Mauricio Junior <junior9771@gmail.com>
2016-11-10 11:57:40 -0200

    Novos métodos, classe TwitterControl

--- Twitter4J/src/br/com/twitte4j/view/Main.java
+++ Twitter4J/src/br/com/twitte4j/view/Main.java
@@ -1,44 +1,47 @@
-package br.com.twitter4j.controller;
+package br.com.twitte4j.view;
 
+import br.com.twitter4j.controller.TwitterConnection;
+import br.com.twitter4j.controller.TwitterControl;
 import twitter4j.QueryResult;
 import twitter4j.Status;
 import twitter4j.Twitter;
 
-public class Snippet {
+public class Main {
 
 	public static void main(String[] args) {
 
 		try {
 
-			TwitterControl twitterControl = new TwitterControl();
-			Twitter twitter = twitterControl.setApiCredentials();
-			String search = "#java9";
-			int cont = 0;
+			TwitterControl control = new TwitterControl();
+			Twitter twitter = TwitterConnection.getApiCredentials();
 
-			System.out.println("1. Quantidade por dia de tweets da última semana");
-			QueryResult result = twitterControl.searchTweetByWeek(twitter, search);
-			for (Status status : result.getTweets()) {
-				if (!status.isRetweeted()) 
-					cont++;
-			}
-			System.out.println("Quantidade de Tweets: " + result.getTweets().size());
-			System.out.println(cont);
+			String busca = "#java9";
+
+			System.out.print("1. Quantidade por dia de tweets da última semana: ");
+			int resultado = control.qtdTwettsUltimaSemana(twitter, busca);
+			System.out.print(resultado + " Tweets criados sobre " + busca + "\n");
 			
-		
+			System.out.print("2. Quantidade por dia de retweets da última semana: ");
+			int resultado2 = control.qtdRetweetsUltimaSemana(twitter, busca);
+			System.out.print(resultado2 + " Tweets criados sobre " + busca + "\n");
 
+			
+			
+			
+			
 			// 2. Quantidade por dia de retweets da última semana.
 			// Como conseguir só os retweets getRetweetCount - isRetweeted
 			// Propriedade do status
 
 			// 3. Quantidade por dia de favoritações da última semana.
 			// favoriteCount
-
-			System.out.println("4. Ordenar os tweets pelo nome do autor, e exibir o primeiro nome e o último nome.");
-			result = twitterControl.orderByName(result);
-			for (Status status : result.getTweets()) {
-				System.out.println("- " + status.getUser().getName() + " \n" + status.getText());
-			}
-
+			/*
+			 * System.out.
+			 * println("4. Ordenar os tweets pelo nome do autor, e exibir o primeiro nome e o último nome."
+			 * ); result = control.orderByName(result); for (Status status :
+			 * result.getTweets()) { System.out.println("- " +
+			 * status.getUser().getName() + " \n" + status.getText()); }
+			 */
 			// 5. Ordenar os tweets por data, e exibir a data mais recente e a
 			// menos recente
 
--- Twitter4J/src/br/com/twitter4j/controller/TwitterControl.java
+++ Twitter4J/src/br/com/twitter4j/controller/TwitterControl.java
@@ -5,6 +5,7 @@ import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.Date;
 import java.util.List;
 
 import twitter4j.Query;
@@ -12,82 +13,66 @@ import twitter4j.QueryResult;
 import twitter4j.Status;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
-import twitter4j.TwitterFactory;
-import twitter4j.auth.AccessToken;
-import twitter4j.conf.Configuration;
-import twitter4j.conf.ConfigurationBuilder;
 
 public final class TwitterControl {
 
-	private final static String apiKey = "LJOe3Qg7ccVNfKkVPaP7Tf848";
-	private final static String apiSecret = "oukrfza9bxmrs8d75pNekkddkBlxNqtbfquCKeI4VRQ2vPzJbC";
-	private final static String accessToken = "790601960890269696-ztYLHfDGufgAK312EZKgbq2aNiGutTW";
-	private final static String accessTokenSecret = "RIYAfooB8G9BNUEBrcMbDE63jUO7f1Cb9kxq3HaK4X2a1";
-
 	/**
-	 * Get Twitter object connection with credentials provided.
+	 * Retornar a qtd de twetts feito ha 7 dias atras ate o dia atual
 	 * 
-	 * @return Twitter object already configured
-	 */
-	public Twitter setApiCredentials() {
-
-		Configuration configuration = loadApiKey().build();
-		AccessToken accessToken = loadAccessToken();
-
-		TwitterFactory factory = new TwitterFactory(configuration);
-		Twitter twitter = factory.getInstance();
-
-		twitter.setOAuthAccessToken(accessToken);
-
-		return twitter;
-	}
-
-	/**
-	 * Configure builder with credentials provided.
-	 * 
-	 * @return ConfigurationBuilder object already configured
-	 */
-	private static ConfigurationBuilder loadApiKey() {
-
-		ConfigurationBuilder builder = new ConfigurationBuilder();
-		builder.setOAuthConsumerKey(TwitterControl.apiKey);
-		builder.setOAuthConsumerSecret(TwitterControl.apiSecret);
-
-		return builder;
-	}
-
-	/**
-	 * Configure access token with credentials provided.
-	 * 
-	 * @return AccessToken object already configured
-	 */
-	private static AccessToken loadAccessToken() {
-
-		return new AccessToken(TwitterControl.accessToken, TwitterControl.accessTokenSecret);
-	}
-
-	/**
-	 *  Return tweets made in the previous week
-	 * 
-	 * @param twitter Twitter object to make the connection
-	 * @param search String with what will be searched
-	 * @return QueryResult object containing result of the search 
+	 * @param Twitter
+	 *            objeto para ser feito a busca no twitter
+	 * @param String
+	 *            com o que sera procurado
+	 * @return QueryResult objeto com o resultado da busca
 	 * @throws TwitterException
 	 */
-	public QueryResult searchTweetByWeek(Twitter twitter, String search) throws TwitterException {
+	public int qtdTwettsUltimaSemana(Twitter twitter, String search) throws TwitterException {
 
-		LocalDate today = LocalDate.now();
-		LocalDate lastWeek = LocalDate.now().minusDays(1);
+		LocalDate ultimoDia = LocalDate.now().minusDays(7);
 		DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-		String s = lastWeek.format(dateTimeFormatter).concat("-").concat(today.format(dateTimeFormatter));
 
-		Query query = new Query(search).since(s);
-		QueryResult result = twitter.search(query);
+		int i = 1;
 
-		return result;
+		Query query = new Query(search);
+		query.count(100);
+		query.since(ultimoDia.format(dateTimeFormatter));
+		QueryResult resultado = twitter.search(query);
+
+		for (Status status : resultado.getTweets()) {
+			if (!status.isRetweet()) {
+				i++;
+			}
+		}
+
+		return i;
 
 	}
 
+	
+	public int qtdRetweetsUltimaSemana(Twitter twitter, String search) throws TwitterException {
+
+		LocalDate ultimoDia = LocalDate.now().minusDays(7);
+		DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+		int i = 1;
+
+		Query query = new Query(search);
+		query.count(100);
+		query.since(ultimoDia.format(dateTimeFormatter));
+		QueryResult resultado = twitter.search(query);
+
+		for (Status status : resultado.getTweets()) {
+			if (status.isRetweet()) {
+				i++;
+			}
+		}
+
+		return i;
+
+	}
+
+	
+	
 	public QueryResult orderByName(QueryResult result) {
 
 		List<Status> list = new ArrayList<>();
